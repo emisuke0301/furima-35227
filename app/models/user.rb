947c8ef82d9580e14d0b30d458302903a658389c
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-04-26 12:06:56 +0900

    バリデーションの設定とエラーメッセージの表示
    This reverts commit 08b5c517167d1a938dce8a24bba2915f3d6c1056.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters" } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "Full-width katakana characters" } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birth_date, presence: true
 end
