b181219e67a1c8c8af13fa0682abb9af7f089850
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-04-26 20:50:17 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,5 +10,9 @@ class ItemsController < ApplicationController
     item.create(item_params)
   end
 
+  private
+	def item_params
+    params.require(:item).permit(:name, :text, :category_id, :status_id, :ship_cost_id, :prefecture_id, :ship_date_id, :price).merge(user_id: current_user.id)
+  end
 
 end
