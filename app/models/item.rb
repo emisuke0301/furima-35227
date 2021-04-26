3281b01dab5f856368580f5263cf01214a920f60
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-04-26 21:18:35 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+	belongs_to :user
+
+	extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+	belongs_to :status
+	belongs_to :ship_cost
+	belongs_to :prefecture
+	belongs_to :ship_date
 end
