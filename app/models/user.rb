bbafc53451813eaf444e03b365717816bfc6f881
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-04-26 18:02:36 +0900

    バリデーションの集約（with_options追加）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
-    validates :last_name
-    validates :first_name
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birth_date
   end
-  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-  validates :birth_date, presence: true
 end
