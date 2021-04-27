5745a1f87c6cd808ce95279e8137e6cb98f159eb
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-04-27 12:31:50 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,18 @@ class Item < ApplicationRecord
 	belongs_to :ship_cost
 	belongs_to :prefecture
 	belongs_to :ship_date
+
+	with_options presence: true do
+		validates :name
+		validates :text
+		validates :price, numericality: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+		validates :image
+	end
+	with_options numericality: { other_than: 1 } do
+		validates :category_id
+		validates :status_id
+		validates :ship_cost_id
+		validates :prefecture_id
+		validates :ship_date_id
+	end
 end
