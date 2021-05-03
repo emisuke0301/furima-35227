7a751d4604eae2fa8a00a07a9d1b07108969abc7
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-05-03 21:04:57 +0900

    Formオブジェクトの訂正

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -1,20 +1,19 @@
 class OrderBuyer
 	include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_num, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_num, :user_id, :item_id
 
 	with_options presence: true do
-		validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
+		validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly" }
 		validates :city
 		validates :block
-		validates :phone_num, numericality: {only_integer: true}, length: { in: 10..11 }
+		validates :phone_num, numericality: { only_integer: true, message: "Input only number" }, length: { in: 10..11 }
 		validates :user_id
 		validates :item_id
-		validates :order_id
 	end
 	validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
 
 	def save
 		order = Order.create(item_id: item_id, user_id: user_id)
-		Buyer.craete(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_num: phone_num, order_id: order_id)
+		Buyer.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_num: phone_num, order_id: order.id)
 	end
 end
