dd503bb4a5a419d0f1c86a29608fd70729e3912e
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-05-03 16:24:53 +0900

    ordersコントローラーの作成・記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,20 @@
+class OrdersController < ApplicationController
+	def index
+		@order_buyer = OrderBuyer.new
+	end
+
+	def create
+		@order_buyer = OrderBuyer.new(order_params)
+		if @order_buyer.valid?
+			@order_buyer.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+	def order_params
+		params.require(:order_buyer).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_num).merge(user_id: current_user.id, item_id: current_item.id, order_id: current_order.id)
+	end
+end
